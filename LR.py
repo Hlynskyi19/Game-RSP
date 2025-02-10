ec4fd2a3c2d36ea81104ba3cc81a40e5253d8ce4
Hlynskyi19 <dimonglina@gmail.com>
2025-02-11 00:46:30 +0200

    Update Lab 12: change while on for

---
+++ LR.py
@@ -0,0 +1,9 @@
+def sum_int_number(number):
+    sum_result = 0
+    for num in range(1, number + 1):
+        sum_result += num
+    return sum_result
+
+
+if __name__ == "__main__":
+    print(sum_int_number(10))
